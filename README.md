3d83ddeeeaf548fc9c2b10da295428d779f4f8e3
Akilan <akil.dove@gmail.com>
2017-11-10 22:42:35 +0530

    Initial Files added

---
+++ README.md
@@ -0,0 +1,1 @@
+# Core Java for Beginners
---
+++ src/com/example/java/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.example.java;
+
+public class HelloWorld {
+	
+	public String greeting;
+	
+	// Main method - It ll get called by default
+	public static void main(String[] args){ 
+		System.out.println("Hello Akilan, Welcome to Java");
+		HelloWorld greeting = new HelloWorld();
+		greeting.test();
+		
+	}
+	
+	//Constructor get called every time when object created
+	public HelloWorld(){
+		System.out.println("Constructor Call");
+		
+	}
+	
+	
+	// User Defined function
+	public void test(){
+		System.out.println("Calling test function");
+	}
+
+}
